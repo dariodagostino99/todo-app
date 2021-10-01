eeebc7e18882799f33fd49287631577364edda12
dariodagostino99 <dagostinodario99@gmail.com>
2021-10-01 11:04:15 -0300

    fixed build error

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,15 +1,3 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
-
-export default function handler(req, res) {
-  if (req.method === 'POST'){
-    res.status(200).send({message:'Good request'})
-    const body = JSON.parse(req.body)
-  }
-
-
-}
